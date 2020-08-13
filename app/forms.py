2668488e74cf47f0e88491c3ab633f954aaa82e3
agnaldoluiz <agnaldoluiz@gmail.com>
2020-08-13 03:25:40 -0300

    UPS added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
     style_pwd = {'class': 'form-control mr-sm-2', 'placeholder': 'Password'}
+    style_email = {'class': 'form-control mr-sm-2', 'placeholder': 'E-mail'}
     style_sbm = {'class': 'btn btn-md btn-secondary'}
+    email = StringField('Email', validators=[Email()], render_kw=style_email)
     password = PasswordField('Password', validators=[DataRequired()], render_kw=style_pwd)
     submit = SubmitField('Access', render_kw=style_sbm)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('contacts.db')
+contacts = pd.read_sql_query("SELECT * from contacts", conn)
+email_senha = pd.Series(contacts.senha.values, index=contacts.email).to_dict()
 #from flask_login import current_user, login_user
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,14 +17,19 @@ def index():
     form = LoginForm()
     if form.validate_on_submit():
         pwd = form.password.data
+        email = form.email.data
         # user = 'random'
-        if pwd == 'Kearney2020':
-            return redirect(url_for('home'))
+        if email in email_senha and pwd == email_senha[email]:
+            return redirect(url_for('ups'))
         else:
             flash('Password Incorrect. Try again')
             # login_user(user)
             return redirect(url_for('index'))
     return render_template('cover.html', form=form)
+
+@app.route('/ups', methods=['GET', 'POST'])
+def ups():
+    return render_template('ups.html')
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
